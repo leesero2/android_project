31ead239e018d1b69897d94411d746a3b65cf25f
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-10 21:13:47 +0900

    회원가입 페이지 제작
    회원가입 xml 디자인 페이지에 뭘 건들여서 에러코드가 안남
    그거때매 실행이 안되서 에러코드 다시 뜨게끔 손을 한번 봐야함

--- android_project/app/src/main/java/com/example/android_project/MainActivity.java
+++ android_project/app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
---
+++ android_project/app/src/main/java/com/example/android_project/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
