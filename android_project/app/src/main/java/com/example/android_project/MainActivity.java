4d876887105c33171e592e9f4119f7ba7d5c9a19
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-12 18:21:22 +0900

    Revert "회원가입 페이지 제작"
    This reverts commit 31ead239e018d1b69897d94411d746a3b65cf25f.

--- android_project/app/src/main/java/com/example/android_project/MainActivity.java
+++ android_project/app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView registerButton = (TextView) findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                MainActivity.this.startActivity(registerIntent);
-            }
-        });
     }
 }
